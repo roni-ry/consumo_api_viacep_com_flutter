bbbb71e91c449b909f8ecaba177d6d8439e6e73c
roni <ronald.dev@gmail.com>
2023-10-22 15:53:00 -0300

    App Flutter Para Consumo da API do ViaCEP

---
+++ README.md
@@ -0,0 +1,3 @@
+# consumo_api_viacep_com_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/consumo_api_viacep_com_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.consumo_api_viacep_com_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
